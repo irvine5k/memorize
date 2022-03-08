b533a3ca712453e1f74caaacc27f48a2692706da
Kelven Galvão <kelvengalvao@gmail.com>
2022-03-08 18:09:38 -0300

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Kelven Galvao on 08/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
